26009341b4be916f4e45800d8d10ee6c9eb8b055
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-29 15:45:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,9 +12,15 @@ introduction("Sammy")
 
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
-  
 end
 
+introduction_with_language("Cat", "JavaScript")
+
+def introduction_with_language (name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Cat")
 
 
 
